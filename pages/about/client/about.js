0563604fed9de5a85d1bc0241fd15af6b926e1ac
Meliodasli <40968604+Meliodasli@users.noreply.github.com>
2018-07-11 10:38:06 -0400

    douhao

--- pages/about/client/about.js
+++ pages/about/client/about.js
@@ -1,10 +1,10 @@
 const team =
   [
-    {name:"Sijun Li"    , email: "sijunjerryli2019@gmail.com" },
+    {name:"Sijun Li", email: "sijunjerryli2019@gmail.com" },
     {name:"Liangzhe Zhao", email:"zhaoliangzhe@gmail.com"},
     {name:"Jiaqi Shao", email:"hzshaojiaqi@163.com"},
     {name:"Jiyang Chen", email:"cjykuwabara@gmail.com"},
-    {name:"土拨鼠"，email:"土拨鼠的email"}
+    {name:"土拨鼠",email:"土拨鼠的email"}
   ];
 
 Template.about.helpers(
