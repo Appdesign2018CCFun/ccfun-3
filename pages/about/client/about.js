72f2ca76bee87a92cbe14d1b43c2042179b83fde
Boomshotgun <41084678+Boomshotgun@users.noreply.github.com>
2018-07-11 10:33:23 -0400

    about 改动
    愚蠢的土拨鼠

--- pages/about/client/about.js
+++ pages/about/client/about.js
@@ -1,16 +1,10 @@
 const team =
   [
     {name:"Sijun Li"    , email: "sijunjerryli2019@gmail.com" },
-<<<<<<< HEAD
-    {name:"Jordan Phillips", email:"jordan.m.phillips23@gmail.com"},
-    {name:"Zeline Bartolome", email:"ztbartolome@brandeis.edu"},
-    {name:"Dongwei Wu", email:"??"}
-=======
     {name:"Liangzhe Zhao", email:"zhaoliangzhe@gmail.com"},
     {name:"Jiaqi Shao", email:"hzshaojiaqi@163.com"},
     {name:"Jiyang Chen", email:"??"},
     {name:"土拨鼠",email:"土拨鼠的email"}
->>>>>>> 065f8f169abf2518dea11e75cfe05e4f3342b426
   ];
 
 Template.about.helpers(
