9c51ac1257001aa78035664e5cda56bfea4b5adc
peanutjam <james078020@gmail.com>
2018-07-11 10:35:09 -0400

    qwrn

--- pages/about/client/about.js
+++ pages/about/client/about.js
@@ -1,11 +1,6 @@
 const team =
   [
     {name:"Sijun Li"    , email: "sijunjerryli2019@gmail.com" },
-<<<<<<< HEAD
-    {name:"Jordan Phillips", email:"jordan.m.phillips23@gmail.com"},
-    {name:"Zeline Bartolome", email:"ztbartolome@brandeis.edu"},
-    {name:"Dongwei Wu", email:"??"}
-=======
     {name:"Liangzhe Zhao", email:"zhaoliangzhe@gmail.com"},
     {name:"Jiaqi Shao", email:"hzshaojiaqi@163.com"},
     {name:"Jiyang Chen", email:"??"},
