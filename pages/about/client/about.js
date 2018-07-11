45d3bc09e1b3ae4b6a8feaadcdce486ea31bfe8c
Boomshotgun <41084678+Boomshotgun@users.noreply.github.com>
2018-07-11 10:19:10 -0400

    改动
    看一下

--- pages/about/client/about.js
+++ pages/about/client/about.js
@@ -1,9 +1,10 @@
 const team =
   [
-    {name:"Tim Hickey"    , email: "tjhickey@brandeis.edu" },
-    {name:"Jordan Phillips", email:"jordan.m.phillips23@gmail.com"},
-    {name:"Zeline Bartolome", email:"ztbartolome@brandeis.edu"},
-    {name:"Dongwei Wu", email:"??"}
+    {name:"Sijun Li"    , email: "sijunjerryli2019@gmail.com" },
+    {name:"Liangzhe Zhao", email:"zhaoliangzhe@gmail.com"},
+    {name:"Jiaqi Shao", email:"hzshaojiaqi@163.com"},
+    {name:"Jiyang Chen", email:"??"},
+    {name:"土拨鼠"，email:"土拨鼠的email"}
   ];
 
 Template.about.helpers(
