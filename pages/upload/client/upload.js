260accb3feb4cd5f6a6708e8225bba232841bf76
nikaido-shinku <cjykuwabara@gmail.com>
2018-07-11 11:41:19 -0400

    upload

--- pages/upload/client/upload.js
+++ pages/upload/client/upload.js
@@ -1,25 +1,24 @@
-Template.uploadprofile.helpers({
-  user(){
-    var theProfile = Profiles.findOne({owner:Meteor.userId()});
-    if (!theProfile){
-      var k = Profiles.find().count();
-      Profiles.insert({name:"anonymous"+k,owner:Meteor.userId()});
-    } else {
-    return theProfile
-    }
-   },
-
-
-})
-
-
-Template.uploadinfo.events({
-  "click #js-submit"(event,instance){
-    const name = instance.$('#js-name').val();
+/* Template.upload.events({
+   "click #js-upload"(event,instance){
+    var k = Video.find().count();
+    k = k + 1
+    Video.insert({title:"unknown"+k, cpath:"unknown"+k, vpath:"unknown"+k, ds:"unknown"+k, qt:"unknown"+k, vid:k});
+    const title = instance.$('#js-title').val();
     console.log('just read '+name);
-    const bday = instance.$('js-bday').val();
-    console.log('just read'+bday);
-    this.user.name = name;
-    Profiles.update(this.user._id,this.user);
+    const cpath = instance.$('js-cpath').val();
+    console.log('just read'+cpath);
+    const vpath = instance.$('js-vpath').val();
+    console.log('just read'+vpath);
+    const ds = instance.$('js-ds').val();
+    console.log('just read'+ds);
+    const qt = instance.$('js-qt').val();
+    console.log('just read'+qt);
+    var theVideo = Video.findOne({vid:k});
+    theVideo.title = title;
+    theVideo.cpath = cpath;
+    theVideo.vpath = vpath;
+    theVideo.ds = ds;
+    theVideo.qt = qt;
+    Video.update(k, theVideo);
   }
 })
