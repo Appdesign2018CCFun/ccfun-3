e8bf04ad2d39d092efd1cd09be338a3cb34697b4
peanutjam <james078020@gmail.com>
2018-07-11 10:37:12 -0400

    asdf

--- pages/about/client/about.js
+++ pages/about/client/about.js
@@ -5,7 +5,6 @@ const team =
     {name:"Jiaqi Shao", email:"hzshaojiaqi@163.com"},
     {name:"Jiyang Chen", email:"??"},
     {name:"土拨鼠",email:"土拨鼠的email"}
->>>>>>> 065f8f169abf2518dea11e75cfe05e4f3342b426
   ];
 
 Template.about.helpers(
