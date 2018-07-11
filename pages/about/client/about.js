ff92aad09960568e467345d8090f8d6c9925fe30
Boomshotgun <41084678+Boomshotgun@users.noreply.github.com>
2018-07-11 10:36:36 -0400

    辣鸡

--- pages/about/client/about.js
+++ pages/about/client/about.js
@@ -3,16 +3,10 @@ const team =
     {name:"Sijun Li"    , email: "sijunjerryli2019@gmail.com" },
     {name:"Liangzhe Zhao", email:"zhaoliangzhe@gmail.com"},
     {name:"Jiaqi Shao", email:"hzshaojiaqi@163.com"},
-<<<<<<< HEAD
     {name:"Jiyang Chen", email:"cjykuwabara@gmail.com"},
     {name:"土拨鼠"，email:"土拨鼠的email"}
-=======
-    {name:"Jiyang Chen", email:"??"},
-    {name:"土拨鼠",email:"土拨鼠的email"}
-<<<<<<< HEAD
-=======
->>>>>>> 5fb708244d1a1810b9110edfda77a3cbf5b3d01e
->>>>>>> 758034d0d6411c8e765ade3e4c6efd3927309159
+
+
   ];
 
 Template.about.helpers(
