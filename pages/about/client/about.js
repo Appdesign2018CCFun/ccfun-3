0dc42c4b2a12c312f264f9e86eecde42cc62e91f
peanutjam <james078020@gmail.com>
2018-07-11 10:29:28 -0400

    kjgfd

--- pages/about/client/about.js
+++ pages/about/client/about.js
@@ -1,6 +1,6 @@
 const team =
   [
-    {name:"Tim Hickey"    , email: "tjhickey@brandeis.edu" },
+    {name:"Sijun Li"    , email: "sijunjerryli2019@gmail.com" },
     {name:"Jordan Phillips", email:"jordan.m.phillips23@gmail.com"},
     {name:"Zeline Bartolome", email:"ztbartolome@brandeis.edu"},
     {name:"Dongwei Wu", email:"??"}
