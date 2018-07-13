1b8f86f47a58902079aa01176cab459f91fdae12
peanutjam <james078020@gmail.com>
2018-07-13 10:53:16 -0400

    afdsfdsa

--- pages/facebook/client/fbprofile.js
+++ pages/facebook/client/fbprofile.js
@@ -12,15 +12,42 @@ Template.fbprofile.helpers({
 
 })
 
+Template.fbinfo.helpers({
+  isSelected(a,b){
+    if (a==b){
+      return "selected"
+    } else {
+      return ""
+    }
+  }
+})
+
+Template.fbinfo.helpers({
+  isSelectedcountry(a,b){
+    if (a==b){
+      return "selected"
+    } else {
+      return ""
+    }
+  }
+})
+
 
 Template.fbinfo.events({
-  "click #js-submit"(event,instance){
-    const name = instance.$('#js-name').val();
-    console.log('just read '+name);
-    const bday = instance.$('#js-bday').val();
-    console.log('just read'+bday);
-    this.user.name = name;
-    this.user.bday = bday;
-    Profiles.update(this.user._id,this.user);
-  }
+    "click #js-submit"(event,instance){
+      const name = instance.$('#js-name').val();
+      console.log('just read '+name);
+      const bday = instance.$('#js-bday').val();
+      console.log('just read'+bday);
+      const gender = instance.$('#js-gender').val();
+      console.log('just read'+ gender);
+      const country = instance.$('#js-country').val();
+      console.log('just read'+ country);
+      this.user.name = name;
+      this.user.bday = bday;
+      this.user.gender = gender;
+      this.user.country = country;
+      Profiles.update(this.user._id,this.user);
+    },
+
 })
